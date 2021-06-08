0d72ea51c27674a1765d70d6857049c4fd5b5a68
unknown <arian_lucas@outlook.com>
2021-06-08 19:27:47 -0300

    Quando precisar se o tamanho da foto serve pra aquele filtro bonito

---
+++ Me_segue_no_insta.js
@@ -0,0 +1,10 @@
+function solucao(largura, altura) {
+    if (largura > altura){
+        console.log("PAISAGEM");
+    } else if (largura == altura){
+        console.log ("QUADRADA");
+    } else {
+        console.log("RETRATO");
+    }
+    
+}
